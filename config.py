4126ddab65bef211836c783a886636b9641f5897
Dragan Vukman <152298859+draganvukman@users.noreply.github.com>
2025-02-02 01:26:19 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 # Database Configuration
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://blog_db_e8gl_user:1j4xYoqvb8I7L6BnDLtp19sbG8uMHxn8@dpg-ct87t652ng1s73adnce0-a/blog_db_e8gl'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://blog_db_osau_user:BsJaD6lftS02sXDAAVJxzOXqW7W6ie60@dpg-cuf860ogph6c7381h7m0-a/blog_db_osau'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
